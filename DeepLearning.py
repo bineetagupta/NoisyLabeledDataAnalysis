d0b1ce0bca2a9fa3569d35eea5e66c353758d900
jaydeepchakraborty <jaydeep.chakraborty.1988@gmail.com>
2017-11-04 16:58:27 -0700

    updated custom loss

--- DeepLearning.py
+++ DeepLearning.py
@@ -1,8 +1,29 @@
 import tensorflow as tf
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 
+
+def custom_loss_func(ground_truth, predictions, rho_pos, rho_neg): 
+    ground_truth = np.array(ground_truth);
+    predictions = np.array(predictions)
+    
+    neg_indices = [i for i,x in enumerate(ground_truth) if x == -1]
+    neg_truth_val = ground_truth[neg_indices]
+    neg_pred_val = predictions[neg_indices]
+    
+    pos_indices = [i for i,x in enumerate(ground_truth) if x == 1]
+    pos_truth_val = ground_truth[pos_indices]
+    pos_pred_val = predictions[pos_indices]
+    
+    neg_Loss = ((1-np.count_nonzero((neg_truth_val == neg_pred_val)))/neg_pred_val.shape[0])
+    pos_Loss = ((1-np.count_nonzero((pos_truth_val == pos_pred_val)))/pos_pred_val.shape[0])
+    
+    cust_loss = (1-rho_neg)*pos_Loss - rho_pos*neg_Loss
+    return cust_loss
+
+
+rho_pos = 0.1
+rho_neg = 0.1
 #train and test files
 train_file = '/Users/jaydeep/jaydeep_workstation/ASU/Fall2017/CSE569_FSL/project/trainData.csv'
 test_file = '/Users/jaydeep/jaydeep_workstation/ASU/Fall2017/CSE569_FSL/project/testData.csv'
@@ -11,23 +32,22 @@ test_file = '/Users/jaydeep/jaydeep_workstation/ASU/Fall2017/CSE569_FSL/project/
 train_data=pd.read_csv(train_file, names=['f1','f2','f3'])
 test_data=pd.read_csv(test_file, names=['f1','f2','f3'])
 
-#map data label into arrays, #specifically for deep learning
-lbl_0=np.asarray([1,0])
-lbl_1=np.asarray([0,1])
-train_data['f3'] = train_data['f3'].map({0: lbl_0, 1: lbl_1})
-test_data['f3'] = test_data['f3'].map({0: lbl_0, 1: lbl_1})
-
 #shuffle the train data
 train_data=train_data.iloc[np.random.permutation(len(train_data))]
 
+#map data label into arrays, #specifically for deep learning
+lbl_0=np.asarray([1,0])
+lbl_1=np.asarray([0,1])
 
 
 #training data
 x_train=train_data[['f1','f2']]
-y_train=train_data['f3']
+y_train_actual=train_data['f3']
+y_train = y_train_actual.map({1: lbl_0, -1: lbl_1})
 #test data
 x_test=test_data[['f1','f2']]
-y_test=test_data['f3']
+y_test_actual=test_data['f3']
+y_test = y_test_actual.map({1: lbl_0, -1: lbl_1})
 
 #placeholders and variables. input has 2 features and output has 2 classes
 x=tf.placeholder(tf.float32,shape=[None,2])
@@ -59,8 +79,10 @@ cross_entropy = tf.reduce_mean(cross_entropy)*100
 #optimiser -
 optimizer = tf.train.AdamOptimizer(0.003).minimize(cross_entropy)
 
-#calculating accuracy of our model 
-is_correct = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+#calculating accuracy of our model
+prediction = tf.argmax(y,1)
+correct_label = tf.argmax(y_,1)
+is_correct = tf.equal(prediction, correct_label)
 accuracy = tf.reduce_mean(tf.cast(is_correct, tf.float32))
 
 #session parameters
@@ -74,11 +96,38 @@ for step in range(epoch):
     feed_dict_train_data = {x: x_train, y_:[t for t in y_train.as_matrix()], pkeep:1}# we kept all the synopsys as we have very less data 
     # train
     sess.run(optimizer, feed_dict=feed_dict_train_data)
-    
+
+y_train_mat = y_train.as_matrix()
+train_predicted = []
+for i in range(0, len(x_train)):
+    train_res = sess.run(prediction, feed_dict={x: [x_train.as_matrix()[i]], pkeep:1})
+    train_res[train_res == 1] = -1
+    train_res[train_res == 0] = 1
+    train_predicted.append(train_res[0])
+
+
+
+train_ground_truth = np.array(y_train_actual.as_matrix())
+train_predictions = np.array(train_predicted)
+print("Training Custom Loss:- ", custom_loss_func(train_ground_truth, train_predictions, rho_pos, rho_neg))
+
 #finding accuracy on testing set
 feed_dict_test = {x: x_test,
                   y_: [t for t in y_test.as_matrix()], pkeep:1} 
 
 acc = sess.run(accuracy, feed_dict=feed_dict_test)
 
-print("Accuracy:- "+str(acc))
+print("Test Accuracy:- "+str(acc))
+
+
+y_test_mat = y_test.as_matrix()
+test_predicted = []
+for i in range(0, len(x_test)):
+    test_res = sess.run(prediction, feed_dict={x: [x_test.as_matrix()[i]], pkeep:1})
+    test_res[test_res == 1] = -1
+    test_res[test_res == 0] = 1
+    test_predicted.append(test_res[0])
+    
+test_ground_truth = np.array(y_test_actual.as_matrix())
+test_predictions = np.array(test_predicted)
+print("Testing Custom Loss:- ", custom_loss_func(test_ground_truth, test_predictions, rho_pos, rho_neg))
--- SVM.py
+++ SVM.py
@@ -1,9 +1,31 @@
+from sklearn import svm
 import numpy as np
-def my_custom_loss_func(ground_truth, predictions):
-    diff = np.abs(ground_truth - predictions).max()
-    return np.log(1 + diff)
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import hinge_loss
+from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score
+
+
+def custom_loss_func(ground_truth, predictions, rho_pos, rho_neg): 
+    ground_truth = np.array(ground_truth);
+    predictions = np.array(predictions)
+    
+    neg_indices = [i for i,x in enumerate(ground_truth) if x == -1]
+    neg_truth_val = ground_truth[neg_indices]
+    neg_pred_val = predictions[neg_indices]
+    
+    pos_indices = [i for i,x in enumerate(ground_truth) if x == 1]
+    pos_truth_val = ground_truth[pos_indices]
+    pos_pred_val = predictions[pos_indices]
+    
+    neg_hingeLoss = hinge_loss(neg_truth_val, neg_pred_val)
+    pos_hingeLoss = hinge_loss(pos_truth_val, pos_pred_val)
+    
+    cust_loss = (1-rho_neg)*pos_hingeLoss - rho_pos*neg_hingeLoss
+    return cust_loss
 
 #http://scikit-learn.org/stable/modules/svm.html
+rho_pos = 0.1
+rho_neg = 0.1
 trainFile = open("/Users/jaydeep/jaydeep_workstation/ASU/Fall2017/CSE569_FSL/project/trainData.csv", "r")
 testFile = open("/Users/jaydeep/jaydeep_workstation/ASU/Fall2017/CSE569_FSL/project/testData.csv", "r")
 
@@ -28,19 +50,28 @@ for line in test_lines:
         testX.append([float(cols[0]),float(cols[1])]);
         testY.append(int(cols[2]));
 
-from sklearn import svm
-from sklearn.metrics import make_scorer
-loss  = make_scorer(my_custom_loss_func, greater_is_better=False)
-score = make_scorer(my_custom_loss_func, greater_is_better=True)
+
 linear_svc = svm.LinearSVC();
 linear_svc.fit(trainX, trainY);
-l = loss(linear_svc,trainX, trainY);
-print(l);
-predictions = linear_svc.predict(testX);
-from sklearn.metrics import confusion_matrix
-print(confusion_matrix(testY, predictions));
-from sklearn.metrics import precision_score,recall_score,f1_score,accuracy_score
-print(precision_score(testY, predictions, average='micro'));
-print(recall_score(testY, predictions, average='micro'));
-print(f1_score(testY, predictions, average='micro'));
-print(accuracy_score(testY, predictions));
+
+# trainingset accuracy
+train_predictions = linear_svc.predict(trainX);
+train_hinge_loss = hinge_loss(trainY, train_predictions);
+trian_cust_acc = custom_loss_func(trainY, train_predictions, rho_pos, rho_neg);
+print("Hinge Train Loss:- ", train_hinge_loss);
+print("Custom Train Loss:- ", trian_cust_acc);
+
+# testingset accuracy
+test_predictions = linear_svc.predict(testX);
+test_hinge_loss = hinge_loss(testY, test_predictions);
+test_cust_acc = custom_loss_func(testY, test_predictions, rho_pos, rho_neg);
+print("Hinge Test Loss:- ", test_hinge_loss);
+print("Custom Test Loss:- ", test_cust_acc);
+
+# Evaluation matrix
+print("Confusion matrix on test data:- ");
+print(confusion_matrix(testY, test_predictions));
+print("Precision Test Score:- ",precision_score(testY, test_predictions, average='micro'));
+print("Recall Test Score:- ",recall_score(testY, test_predictions, average='micro'));
+print("F1 Test Score:- ",f1_score(testY, test_predictions, average='micro'));
+print("Accuracy Test Score:- ",accuracy_score(testY, test_predictions));
