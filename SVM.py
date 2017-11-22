603650991b92f3b022cfe06d02e61d621dfbc758
jaydeepchakraborty <jaydeep.chakraborty.1988@gmail.com>
2017-11-21 19:40:33 -0700

    SVM with loss

--- SVM.py
+++ SVM.py
@@ -1,72 +1,160 @@
 from sklearn import svm
 import numpy as np
-from sklearn.metrics import hinge_loss,zero_one_loss
+from sklearn.metrics import hinge_loss,zero_one_loss, log_loss
+from sklearn.metrics.scorer import make_scorer
+from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import confusion_matrix, precision_score, recall_score, f1_score, accuracy_score
 
+class noisyLblSVM(object):
 
-def custom_loss_func(ground_truth, prediction, rho_pos, rho_neg): 
-
-    hingeLoss_1 = hinge_loss([ground_truth], [prediction])
-    hingeLoss_2 = hinge_loss([ground_truth], [-prediction])
-    if(prediction == 1):
-        cust_loss = ((1-rho_neg)*hingeLoss_1 - rho_pos*hingeLoss_2)/(1- rho_pos - rho_neg)
-    else:
-        cust_loss = ((1-rho_pos)*hingeLoss_1 - rho_neg*hingeLoss_2)/(1- rho_pos - rho_neg)
-    
-    return cust_loss
-
-
-def calSVM(rho_pos_val, rho_neg_val, trainFileNm, testFileNm):
-    #http://scikit-learn.org/stable/modules/svm.html
-    rho_pos = rho_pos_val
-    rho_neg = rho_neg_val
-    trainFile = open(trainFileNm, "r")
-    testFile = open(testFileNm, "r")
-    
-    
-    train_lines = trainFile.read().split("\n") # "\r\n" if needed
-    trainX = [];
-    trainY = [];
-    
-    for line in train_lines:
-        if line != "": # add other needed checks to skip titles
-            cols = line.split(",")
-            trainX.append([float(cols[0]),float(cols[1])]);
-            trainY.append(int(cols[2]));
-            
-    test_lines = testFile.read().split("\n") # "\r\n" if needed       
-    testX = [];
-    testY = [];
-    
-    for line in test_lines:
-        if line != "": # add other needed checks to skip titles
-            cols = line.split(",")
-            testX.append([float(cols[0]),float(cols[1])]);
-            testY.append(int(cols[2]));
-    
-    
-    linear_svc = svm.LinearSVC();
-    linear_svc.fit(trainX, trainY);
-    
-    
-    # testingset accuracy
-    test_predictions = linear_svc.predict(testX);
-    test_hinge_loss = hinge_loss(testY, test_predictions);
-    test_cust_acc = 0;
-    for i in range(0, len(testY)):
-        test_cust_acc += custom_loss_func(testY[i], test_predictions[i], rho_pos, rho_neg);
+    def __init__(self):
+        self._rho_pos = 0
+        self._rho_neg = 0
         
-    mean_test_cust_acc = np.mean(test_cust_acc)
+    @property
+    def rho_pos(self):
+        print("getter of rho_pos called")
+        return self._rho_pos
+
+    @rho_pos.setter
+    def rho_pos(self, value):
+        print("setter of rho_pos called")
+        self._rho_pos = value
+
+    @property
+    def rho_neg(self):
+        print("getter of rho_neg called")
+        return self._rho_neg
+
+    @rho_neg.setter
+    def rho_neg(self, value):
+        print("setter of rho_neg called")
+        self._rho_neg = value
+
+    def custom_loss_func(self,ground_truth, prediction): 
+#         print('custom', '-------------------')  
+        #˜l(t, y) = ( (1 − ρ(−y))l(t, y) − ρ(y)l(t,−y) ) / ( 1 - ρ(−y) - ρ(y))
+        cust_loss = 0
+#         labels = np.array([-1, 1])
+        #rho_pos is ρ(y) and rho_neg is ρ(−y)  
+        for i in range(0, len(ground_truth)):
+            #Here l(t, y) is considered as SKLearn inbuilt hinge loss
+#             print([ground_truth[i]])
+#             print([prediction[i]])
+            hingeLoss_1 = zero_one_loss([ground_truth[i]], [prediction[i]]) #l(t, y)
+            hingeLoss_2 = zero_one_loss([ground_truth[i]], [-prediction[i]]) #l(t,−y)
+             
+            if(prediction[i] == 1):
+#                 print([ground_truth[i]], [prediction[i]], ((1-self._rho_neg)*hingeLoss_1 - self._rho_pos*hingeLoss_2)/(1- self._rho_pos - self._rho_neg))
+                cust_loss += ((1-self._rho_neg)*hingeLoss_1 - self._rho_pos*hingeLoss_2)/(1- self._rho_pos - self._rho_neg)
+            else:
+#                 print([ground_truth[i]], [prediction[i]], ((1-self._rho_pos)*hingeLoss_1 - self._rho_neg*hingeLoss_2)/(1- self._rho_pos - self._rho_neg))
+                cust_loss += ((1-self._rho_pos)*hingeLoss_1 - self._rho_neg*hingeLoss_2)/(1- self._rho_pos - self._rho_neg)
+            
+#             print(cust_loss)
+        return cust_loss
     
-#     print("Hinge Test Loss:- ", test_hinge_loss)
-#     print("Custom Test Loss:- ", mean_test_cust_acc)
+    def org_loss_func(self,ground_truth, prediction):
+#         print('original', len(ground_truth))
+        #print('------------------')
+        org_loss = 0
+        for i in range(0, len(ground_truth)):
+            #print([ground_truth[i]], [prediction[i]], zero_one_loss([ground_truth[i]], [prediction[i]]))
+            #print('org_loss', org_loss)
+            org_loss += zero_one_loss([ground_truth[i]], [prediction[i]])
+            
+        return org_loss
+        
     
-    # Evaluation matrix
-#     print("Confusion matrix on test data:- ")
-#     print(confusion_matrix(testY, test_predictions))
-    precision_val = precision_score(testY, test_predictions, average='micro')
-    recall_val = recall_score(testY, test_predictions, average='micro')
-    f1_val = f1_score(testY, test_predictions, average='micro')
-    acc_val = accuracy_score(testY, test_predictions)
-    
-    return test_hinge_loss, mean_test_cust_acc, precision_val, recall_val, f1_val, acc_val
+    def calSVM(self,rho_pos_val, rho_neg_val, trainFileNm, testFileNm):
+        #http://scikit-learn.org/stable/modules/svm.html
+#         labels = np.array([-1, 1])
+        #print(zero_one_loss([1], [1]))
+        #print(zero_one_loss([1], [-1]))
+        #print(zero_one_loss([-1], [1]))
+        #print(zero_one_loss([-1], [-1]))
+        #assigning rho(+y) and rho(-y) 
+        self._rho_pos = rho_pos_val
+        self._rho_neg = rho_neg_val
+        #Opening training file which is flipped by rho_pos and rho_neg.
+        trainFile = open(trainFileNm, "r")
+        #Opening testing file which is not flipped.
+        testFile = open(testFileNm, "r")      
+        
+        #populating training trainX(features) and trainY(labels)
+        train_lines = trainFile.read().split("\n") # "\r\n" if needed
+        trainX = [];
+        trainY = [];
+        
+        for line in train_lines:
+            if line != "": # add other needed checks to skip titles
+                cols = line.split(",")
+                trainX.append([float(cols[0]),float(cols[1])]);
+                trainY.append(int(cols[2]));
+         
+         
+        #populating training testX(features) and testY(labels)       
+        test_lines = testFile.read().split("\n") # "\r\n" if needed       
+        testX = [];
+        testY = [];
+        
+        for line in test_lines:
+            if line != "": # add other needed checks to skip titles
+                cols = line.split(",")
+                testX.append([float(cols[0]),float(cols[1])]);
+                testY.append(int(cols[2]));
+        
+        
+        org_scorer = make_scorer(self.org_loss_func, greater_is_better=False)
+        
+        #Using SKLearn inbuilt svm library
+        svc_model_org = GridSearchCV(svm.SVC(kernel='rbf', gamma=0.1),
+                       scoring=org_scorer,
+                       cv=5,
+                       param_grid={"C": [1e0, 1e1]})
+#         svc_model_org = svm.SVC()
+        #train the model with flipped training data
+        svc_model_org = svc_model_org.fit(trainX, trainY);
+        
+        print('training score',  svc_model_org.score(trainX,trainY))
+        #Predict the test data labels by using the same model.
+#         test_predictions_org = svc_model_org.predict(testX);
+        
+        #Calculating custom loss of each of the test data
+        #test_cust_acc_org = self.org_loss_func(testY, test_predictions_org)
+#         test_cust_acc_org = svc_model_org.score(testX,testY)
+        test_cust_acc_org = svc_model_org.score(testX,testY)
+        print('testing score', test_cust_acc_org)
+      
+        
+        #Calculating mean of the total custom loss     
+        mean_test_cust_acc_org = test_cust_acc_org / len(testY)
+        
+#         print(accuracy_score(testY, test_predictions_org, normalize=False))
+        
+        cust_scorer = make_scorer(self.custom_loss_func, greater_is_better=False)
+        svc_model_cust = GridSearchCV(svm.SVC(kernel='rbf', gamma=0.1),
+               scoring=cust_scorer,
+               cv=5,
+               param_grid={"C": [1e0, 1e1]})
+        
+        
+        #train the model with flipped training data
+        svc_model_cust = svc_model_cust.fit(trainX, trainY); 
+        print('training score',  svc_model_cust.score(trainX,trainY))       
+        
+        #Predict the test data labels by using the same model.
+        test_predictions_cust = svc_model_cust.predict(testX);
+        
+        #Calculating custom loss of each of the test data
+        #test_cust_acc_cust = self.custom_loss_func(testY, test_predictions_cust)
+        test_cust_acc_cust = svc_model_cust.score(testX,testY)
+#         test_cust_acc_cust = self.org_loss_func(testY,test_predictions_cust)
+        print('testing score',  test_cust_acc_cust)
+        
+        #Calculating mean of the total custom loss     
+        mean_test_cust_acc_cust = test_cust_acc_cust / len(testY)
+        
+#         print(accuracy_score(testY, test_predictions_cust, normalize=False))
+        
+        return mean_test_cust_acc_org, mean_test_cust_acc_cust
--- projectMain.py
+++ projectMain.py
@@ -1,38 +1,42 @@
 import csv
-from SVM import calSVM
+from SVM import noisyLblSVM
 from DeepLearning import calNN
 
 svmOpFile = open('svmOpFile.csv', 'w')
 
-nrs = [[0.1,0.2],[0.6,0.9],[0.5,0.6],[0.8,0.3],[0.8,0.7]];
+nrs = [[0.4,0.4]];
 
 path = '/Users/jaydeep/jaydeep_workstation/ASU/Fall2017/CSE569_FSL/project/';          
-datasets_num = 4;
-nr_num = 5;
+datasets_num = 1;
+nr_num = 1;
 with svmOpFile:
     writer = csv.writer(svmOpFile)
-    val = ['Data', 'NR_pos', 'NP_neg', 'Hinge Loss', 'Custom Accuracy Val', 'Precision Val', 'Recall Val', 'F1 Val', 'Accuracy Val']
+    val = ['DataSet', 'NR_pos', 'NP_neg', 'Original Loss', 'Custom Loss']
     writer.writerows([val])
     for i in range(1, datasets_num+1):
         test_filename = path + 'testData_'+str(i)+'.csv'
+        print(test_filename)
         for j in range(1, nr_num+1):
             train_filename = path + 'DS'+str(i)+'_NR'+str(j)+'_flippedTrainData.csv'
+            print(train_filename)
             rho = nrs[j-1];
-            test_hinge_loss, mean_test_cust_acc, precision_val, recall_val, f1_val, acc_val = calSVM(rho[0],rho[1],train_filename,test_filename);
-            val = [str(i), str(rho[0]), str(rho[1]), test_hinge_loss, mean_test_cust_acc, precision_val, recall_val, f1_val, acc_val]
+            cls = noisyLblSVM()
+            mean_test_cust_acc_org, mean_test_cust_acc_cust = cls.calSVM(rho[0],rho[1],train_filename,test_filename);
+            val = [str(i), str(rho[0]), str(rho[1]), mean_test_cust_acc_org, mean_test_cust_acc_cust]
+            print(val)
             writer.writerows([val])
             
-nnOpFile = open('nnOpFile.csv', 'w')
-with nnOpFile:
-    writer = csv.writer(nnOpFile)
-    val = ['Data', 'NR_pos', 'NP_neg', 'Entropy Loss', 'Custom Accuracy Val', 'Precision Val', 'Recall Val', 'F1 Val', 'Accuracy Val']
-    writer.writerows([val])
-    for i in range(1, datasets_num+1):
-        test_filename = path + 'testData_'+str(i)+'.csv'
-        for j in range(1, nr_num+1):
-            train_filename = path + 'DS'+str(i)+'_NR'+str(j)+'_flippedTrainData.csv'
-            rho = nrs[j-1];
-            emtropy_loss, mean_test_cust_acc, precision_val, recall_val, f1_val, acc_val = calNN(rho[0],rho[1],train_filename,test_filename);
-            val = [str(i), str(rho[0]), str(rho[1]), str(emtropy_loss), mean_test_cust_acc, precision_val, recall_val, f1_val, acc_val]
-            writer.writerows([val])
-    
+# nnOpFile = open('nnOpFile.csv', 'w')
+# with nnOpFile:
+#     writer = csv.writer(nnOpFile)
+#     val = ['DataSet', 'NR_pos', 'NP_neg', 'Entropy Loss', 'Custom Loss Val', 'Precision Val', 'Recall Val', 'F1 Val', 'Accuracy Val']
+#     writer.writerows([val])
+#     for i in range(1, datasets_num+1):
+#         test_filename = path + 'testData_'+str(i)+'.csv'
+#         for j in range(1, nr_num+1):
+#             train_filename = path + 'DS'+str(i)+'_NR'+str(j)+'_flippedTrainData.csv'
+#             rho = nrs[j-1];
+#             emtropy_loss, mean_test_cust_acc, precision_val, recall_val, f1_val, acc_val = calNN(rho[0],rho[1],train_filename,test_filename);
+#             val = [str(i), str(rho[0]), str(rho[1]), str(emtropy_loss), mean_test_cust_acc, precision_val, recall_val, f1_val, acc_val]
+#             writer.writerows([val])
+#     
